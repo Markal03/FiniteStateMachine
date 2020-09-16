64e1eadc8ad793973481138cfa38f72afbde9aa9
Christopher Acornley <c.acornley@abertay.ac.uk>
2020-09-16 15:38:31 +0100

    Added Project and code

---
+++ Week3-FSM/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	bool program_running = true;
+
+	cout << "Starting Finite State Machine" << endl;
+
+	do {
+
+		/*
+		Write your code here for the finite state machine example
+		*/
+
+	} while(program_running);
+
+	cout << "Ending Finite State Machine" << endl;
+
+	return 0;
+}
