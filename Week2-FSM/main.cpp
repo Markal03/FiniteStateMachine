7c1bea6fc276022d7faf08b125fcae67d51444e8
Christopher Acornley <c.acornley@abertay.ac.uk>
2020-09-16 16:00:33 +0100

    Added keyboard handling (very basic) and timer threads

--- Week2-FSM/main.cpp
+++ Week2-FSM/main.cpp
@@ -1,6 +1,11 @@
-#include <iostream>
+#include <iostream>		
+#include <thread>	 
+#include <chrono>
+#include <conio.h>
 
-using namespace std;
+using namespace std;			// cout, endl
+using namespace this_thread;	// sleep_for
+using namespace chrono;			// seconds, milliseconds
 
 int main()
 {
@@ -11,8 +16,24 @@ int main()
 	do {
 
 		/*
+
 		Write your code here for the finite state machine example
+
 		*/
+
+		// Sleep the current thread for 1000 milliseconds. Can be repalce with seconds(1)
+		sleep_for(milliseconds(1000));
+
+		if (_kbhit())
+		{
+			char input_char = _getch();
+
+			// Check for ESC key. See table here http://www.asciitable.com/
+			if (input_char == 27)
+			{
+				program_running = false;
+			}
+		}
 
 	} while(program_running);
 
